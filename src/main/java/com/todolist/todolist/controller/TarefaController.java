fe40361a312339dd487db0c75a590e26a83336d5
LucasRP <rodrigues.lrp21@gmail.com>
2025-06-18 09:02:24 -0300

    atualização em TarefaController: adequação nos métodos para suportar respostas http com ResponseEntity e adição de tratamento de exceção em deletar tarefa

--- src/main/java/com/todolist/todolist/controller/TarefaController.java
+++ src/main/java/com/todolist/todolist/controller/TarefaController.java
@@ -1,10 +1,12 @@
 package com.todolist.todolist.controller;
 
+import com.todolist.todolist.exceptions.TarefaNotFoundException;
 import com.todolist.todolist.model.dto.RequestTarefaDTO;
 import com.todolist.todolist.model.dto.ResponseTarefaDTO;
-import com.todolist.todolist.model.entity.Tarefa;
 import com.todolist.todolist.service.TarefaService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -21,28 +23,39 @@ public class TarefaController {
     }
 
     @GetMapping
-    public List<ResponseTarefaDTO> listarTarefasDia(@RequestParam("dia") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dia){
-        return tarefaService.listarTarefaDodia(dia);
+    public ResponseEntity<List<ResponseTarefaDTO>> listarTarefasDia(@RequestParam("dia") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dia){
+        List<ResponseTarefaDTO> tarefas = tarefaService.listarTarefaDodia(dia);
+
+        if(tarefas.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(tarefas);
     }
 
-    @PostMapping
-    public Tarefa salvarTarefa(@RequestBody RequestTarefaDTO requestTarefaDTO){
-        return tarefaService.criarTarefa( requestTarefaDTO);
+    @PostMapping("/salvar")
+    public ResponseEntity<ResponseTarefaDTO> salvarTarefa(@RequestBody RequestTarefaDTO requestTarefaDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.criarTarefa(requestTarefaDTO));
     }
 
     @PostMapping("/{id}")
-    public ResponseTarefaDTO atualizarTarefa(@PathVariable Long id, @RequestBody RequestTarefaDTO tarefa){
-        return tarefaService.atualizarTarefa(id, tarefa);
+    public ResponseEntity<ResponseTarefaDTO> atualizarTarefa(@PathVariable Long id, @RequestBody RequestTarefaDTO tarefa){
+        return ResponseEntity.ok(tarefaService.atualizarTarefa(id, tarefa));
     }
 
-    @PostMapping("/{status}")
-    public boolean atualizarTarefaStatus(@PathVariable Long id, @RequestParam boolean status){
-        return tarefaService.atualzizarStatusTarefa(id,status);
+    @PostMapping
+    public ResponseEntity<Boolean> atualizarTarefaStatus(@RequestParam Long id, @RequestParam boolean status){
+        return ResponseEntity.ok(tarefaService.atualzizarStatusTarefa(id,status));
     }
 
     @DeleteMapping("/{id}")
-    public void deletarTarefa(@PathVariable Long id){
-        tarefaService.deletarTarefa(id);
+    public ResponseEntity<Void> deletarTarefa(@PathVariable Long id){
+        try {
+            tarefaService.deletarTarefa(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (TarefaNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 
