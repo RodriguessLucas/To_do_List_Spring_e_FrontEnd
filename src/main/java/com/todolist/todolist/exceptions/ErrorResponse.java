65d551d32afc617b848f310a470cf0ddaadb7a3f
LucasRP <rodrigues.lrp21@gmail.com>
2025-06-18 05:35:35 -0300

    feat: criação do ErrorResponse

--- src/main/java/com/todolist/todolist/exceptions/ErrorResponse.java
+++ src/main/java/com/todolist/todolist/exceptions/ErrorResponse.java
@@ -1,4 +1,47 @@
 package com.todolist.todolist.exceptions;
 
 public class ErrorResponse {
+    private int status;
+    private String mensagem;
+    private String metodo;
+    private String caminho;
+
+    public ErrorResponse(int status, String mensagem, String metodo, String caminho){
+        this.status = status;
+        this.mensagem = mensagem;
+        this.metodo = metodo;
+        this.caminho = caminho;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
 }
